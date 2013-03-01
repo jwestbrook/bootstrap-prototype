820b0dbd728839e7d66c3adf49473c411b90dc61
Jason Westbrook <jwestbrook@gmail.com>
2013-03-01 15:43:38 -0800

    version bump to 2.3.1

--- js/bootstrap-collapse_prototype.js
+++ js/bootstrap-collapse_prototype.js
@@ -1,5 +1,5 @@
 /* =============================================================
- * bootstrap-collapse.js v2.2.1
+ * bootstrap-collapse.js v2.3.1
  * http://twitter.github.com/bootstrap/javascript.html#collapse
  * =============================================================
  * Copyright 2012 Twitter, Inc.
